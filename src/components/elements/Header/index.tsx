c960b0ee26abd071a2930adf33c1b82d0edf16dd
rifqifarelmuhammad <rifqifarel222@gmail.com>
2023-10-07 16:27:56 +0700

    Prettier

--- src/components/elements/Header/index.tsx
+++ src/components/elements/Header/index.tsx
@@ -1,8 +1,8 @@
-import Head from "next/head";
+import Head from 'next/head'
 
 export const Header: React.FC = () => (
-    <Head>
-        <title>Simple Todolist</title>
-        <meta property='og:title' content='Simple Todolist' key='title' />
-    </Head>
+  <Head>
+    <title>Simple Todolist</title>
+    <meta property="og:title" content="Simple Todolist" key="title" />
+  </Head>
 )
